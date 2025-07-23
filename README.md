bc5faa5698ef6599432e75af40863f3efcb59929
Jmangle93 <Jmangle93@gmail.com>
2025-07-22 22:00:13 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# SQLite Demo with Poetry
+This project was made for two primary learning objectives in line with parts of two Pluralsight courses: 
+- SQLite with Python, simple database management
+- Poetry for Python dependency management and packaging
+
+It's a simple set of functions that fetch cryptocurrency prices and allow you to add investments from/to a SQLite database.
+SQLite, because I'm new to SQL/database management in general and it doesn't require a server. Poetry is overkill for such a small project, but it seems great for robust application development and I want to learn it.
+
+## Supporting Docs and Tools
+- [Coin Gecko API](coingecko.com/en/api)
+- [SQLite](https://www.sqlite.org/index.html)
+    - [VS Code extension for SQLite](https://marketplace.visualstudio.com/items?itemName=alexcvzz.vscode-sqlite)
+    - [VS Code extension REST book](https://marketplace.visualstudio.com/items?itemName=tanhakabir.rest-book)
+- [poetry](https://python-poetry.org/)
+    - [pipx](https://pipx.pypa.io/stable/installation/)
+    - [PEP 508](https://peps.python.org/pep-0508/)
+    - [PEP 440](https://peps.python.org/pep-0440/)
+    - [VS Code extension for Python](https://marketplace.visualstudio.com/items?itemName=ms-python.python)
+
+## Source Education
+[Working with Databases in Python3 by Douglas Starnes on Pluralsight](https://app.pluralsight.com/library/courses/python-3-working-databases)
+[Development Environments and Package Management in Python3 by Reindert-Jan Ekker on Pluralsight](https://app.pluralsight.com/library/courses/python-3-development-environments-package-management)
---
+++ src/sqlite_demo_poetry/get_price.py
@@ -0,0 +1,58 @@
+import datetime
+import requests
+import sqlite3
+
+CREATE_INVESTMENTS_SQL = """
+CREATE TABLE IF NOT EXISTS investments (
+    coin_id TEXT NOT NULL,
+    vs_currency TEXT NOT NULL,
+    amount REAL,
+    sell INT,
+    date TIMESTAMP
+);
+"""
+
+def get_coin_price(coin_id, vs_currency):
+    url = f'https://api.coingecko.com/api/v3/simple/price?ids={coin_id}&vs_currencies={vs_currency}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        price = response.json()[coin_id][vs_currency]
+        print(f"The price of {coin_id} is ${price} in {vs_currency}.")
+        return price
+    else:
+        raise Exception(f"Error fetching data: {response.status_code}")
+
+def add_investment(coin_id, vs_currency, amount, sell):
+    sql = "INSERT INTO investments VALUES (?, ?, ?, ?, ?);"
+    values = (coin_id, vs_currency, amount, sell, datetime.datetime.now())
+    cursor.execute(sql, values)
+    database.commit()
+    if sell:
+        print(f"Added sell, {amount} of {coin_id} at {vs_currency}.")
+    else:
+        print(f"Added buy, {amount} of {coin_id} at {vs_currency}.")
+
+def get_investment_value(coin_id, vs_currency):
+    price = get_coin_price(coin_id, vs_currency)
+    sql = """SELECT amount
+    FROM investments
+    WHERE coin_id = ? 
+    AND vs_currency = ?
+    AND sell=?;"""
+    buy_result = cursor.execute(sql, (coin_id, vs_currency, False)).fetchall()
+    sell_result = cursor.execute(sql, (coin_id, vs_currency, True)).fetchall()
+    buy_amount = sum([row[0] for row in buy_result])
+    sell_amount = sum([row[0] for row in sell_result])
+
+    total_amount = buy_amount - sell_amount
+    total_value = total_amount * price
+    print(f"You own a total of {total_amount} {coin_id} worth ${total_value} at the current price of ${price} in {vs_currency.upper()}.")
+
+database = sqlite3.connect('portfolio.db')
+cursor = database.cursor()
+cursor.execute(CREATE_INVESTMENTS_SQL)
+
+# Example usage
+# get_coin_price('bitcoin', 'usd')
+# add_investment('bitcoin', 'usd', 0.01, False)
+# get_investment_value('bitcoin', 'usd')
